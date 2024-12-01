aa9f63de1bf75a71440f0cba6c0865178b737f69
Esra <esraayaareb@gmail.com>
2024-12-01 22:09:58 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import logo from "/logo.svg";
+import iconFacebook from "/icon-facebook.svg";
+import iconInstagram from "/icon-instagram.svg";
+import iconTwitter from "/icon-twitter.svg";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="container">
+        <img src={logo} alt="Logo" />
+
+        <div className="copyrights text-5">
+          <p>All rights reserved © Equalizer 2021</p>
+          <p>Have any problems? Contact us via social media or email us at </p>
+          <p className="fw-bold">equalizer@example.com</p>
+        </div>
+
+        <div className="icons">
+          <img src={iconFacebook} alt="Facebook" />
+          <img src={iconInstagram} alt="Instagram" />
+          <img src={iconTwitter} alt="Twitter" />
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import illustration from "/illustration-app.png";
+import pattern1 from "/bg-pattern-1.svg";
+import pattern2 from "/bg-pattern-2.svg";
+import bgMainDesktop from "/bg-main-desktop.png";
+import bgMainTablet from "/bg-main-tablet.png";
+import bgMainMobile from "/bg-main-mobile.png";
+import iconApple from "/icon-apple.svg";
+import iconAndroid from "/icon-android.svg";
 
 export default function Main() {
-  return <main>Main</main>;
+  return (
+    <main>
+      <div className="container">
+        <div className="flex bg-slate">
+          <img
+            src={illustration}
+            alt="illustration"
+            className="illustration-app"
+          />
+
+          <div className="card bg-orange">
+            <h2 className="text-3">Premium EQ</h2>
+            <p className="text-4">
+              Get expert-level control with a robust equalizer, volume mixer,
+              and spatial audio. Take your listening experience to a whole new
+              level and access all our incredible features!
+            </p>
+            <div className="price">
+              <span className="text-2">$4</span>
+              <span className="text-4"> / month</span>
+            </div>
+            <a className="btn bg-slate mb-4 ">
+              <img src={iconApple} alt="iOS" />
+              <span className="text-white text-6">iOS Download</span>
+            </a>
+
+            <a className="btn bg-white">
+              <img src={iconAndroid} alt="Android" />
+              <span className="text-slate text-6">Android Download</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
