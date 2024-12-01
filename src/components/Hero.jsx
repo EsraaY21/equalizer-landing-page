ed3d5da6113b2ecc21f00ec9f73aceb5ec9d0d9c
Esra <esraayaareb@gmail.com>
2024-12-01 20:30:19 +0300

    init

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import logo from "/logo.svg";
+
+export default function Hero() {
+  return (
+    <header>
+      <div className="container">
+        <img src={logo} alt="Logo" />
+
+        <h1 className="text-1">We make your music sound extraordinary</h1>
+        <p className="text-4">
+          A system audio equalizer specifically designed for Android and iOS.
+          Freely tune the way your music sounds with a professional grade
+          parametric EQ & volume mixer. Control bass, mids, treble, gain
+          control, reverb, and more!
+        </p>
+      </div>
+    </header>
+  );
+}
